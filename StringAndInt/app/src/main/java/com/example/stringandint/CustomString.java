e154ad1d430454f8d9bd51675b7e3e732844ea9d
c4arl0s <c.santiago.cruz@gmail.com>
2019-09-26 21:23:05 -0500

    Add project

---
+++ StringAndInt/app/src/main/java/com/example/stringandint/CustomString.java
@@ -0,0 +1,22 @@
+package com.example.stringandint;
+
+public class CustomString {
+    public static void main(String[] args) {
+        String name = "Carlos Santiago";
+                System.out.println(name);
+
+                String anotherName;
+        anotherName = "Santiago Cruz";
+        System.out.println(anotherName);
+
+            int integer = 23;
+        System.out.println(integer);
+
+        int anotherInt;
+        anotherInt = 32;
+        System.out.println(anotherInt);
+
+        int multiplicationInt = integer * anotherInt;
+        System.out.println(multiplicationInt);
+    }
+}
